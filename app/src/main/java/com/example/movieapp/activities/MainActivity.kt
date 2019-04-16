2a04cbe14ddbb2d1c482d8ced1bb45c802be1b19
BBogucki <bartosz.bogucki@hemmersbach.com>
2019-04-16 14:35:51 +0200

    initial commit

---
+++ app/src/main/java/com/example/movieapp/activities/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.movieapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpToolbarMain()
+        setUpBottomNavigationView()
+    }
+
+    private fun setUpToolbarMain() {
+        setSupportActionBar(toolbar_main_id)
+        title = getString(R.string.app_name)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
+    }
+
+    private fun setUpBottomNavigationView() {
+        val navController = findNavController(R.id.my_nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+
+}
+

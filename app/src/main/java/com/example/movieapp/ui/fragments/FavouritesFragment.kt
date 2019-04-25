f9414bee5cd841f137ae5c6ed8a3466a311d2eb6
Adam Czajkowski <adam.czajkowski8@gmail.com>
2019-04-25 12:19:55 +0200

    favorite fragment done, enter detail activity from search and favorite fragment done

--- app/src/main/java/com/example/movieapp/ui/fragments/FavouritesFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/fragments/FavouritesFragment.kt
@@ -6,13 +6,10 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.movieapp.R
-import com.example.movieapp.models.Result
 import com.example.movieapp.ui.activities.DetailsActivity
 import com.example.movieapp.ui.adapters.MovieRecyclerAdapter
 import com.example.movieapp.view.model.favorite.FavoriteViewModel
